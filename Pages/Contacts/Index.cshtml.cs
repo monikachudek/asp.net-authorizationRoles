7758b1029d9cf006a3e3c1757be35eb44838ffb7
Monika <monika.chudek1@gmail.com>
2020-02-13 10:41:48 +0100

    initial commit

---
+++ Pages/Contacts/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using authorizationRoles.Data;
+using authorizationRoles.Models;
+
+namespace authorizationRoles.Pages.Contacts
+{
+    public class IndexModel : PageModel
+    {
+        private readonly authorizationRoles.Data.ApplicationDbContext _context;
+
+        public IndexModel(authorizationRoles.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Contact> Contact { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Contact = await _context.Contact.ToListAsync();
+        }
+    }
+}
